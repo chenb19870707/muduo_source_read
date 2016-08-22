4d43a85f6f29fd2765dbe540324e19f3f1a88851
chenbin <chenb19870707@gmail.com>
2016-08-22 16:51:17 +0800

    原子操作

---
+++ jmuduo/muduo/base/Timestamp.h
@@ -0,0 +1,124 @@
+#ifndef MUDUO_BASE_TIMESTAMP_H
+#define MUDUO_BASE_TIMESTAMP_H
+
+#include <muduo/base/copyable.h>
+#include <muduo/base/Types.h>
+
+#include <boost/operators.hpp>
+
+namespace muduo
+{
+
+///
+/// Time stamp in UTC, in microseconds resolution.
+///
+/// This class is immutable.距
+/// It's recommended to pass it by value, since it's passed in register on x64.
+///
+/*
+ * 距1970-00-00 00:00:00 的微秒数
+ *
+ * copyable 空基类，是一个表示类，继承自copyable的类都是值类型
+ *
+ * less_than_comparable 要求实现 < ，可自动实现 >,<=,>=,模板元编程思想
+ *
+ */
+class Timestamp : public muduo::copyable,
+                  public boost::less_than_comparable<Timestamp>
+{
+ public:
+  ///
+  /// Constucts an invalid Timestamp.
+  ///
+  Timestamp()
+    : microSecondsSinceEpoch_(0)
+  {
+  }
+
+  ///
+  /// Constucts a Timestamp at specific time
+  ///
+  /// @param microSecondsSinceEpoch
+  explicit Timestamp(int64_t microSecondsSinceEpochArg)
+    : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
+  {
+  }
+
+  void swap(Timestamp& that)
+  {
+    std::swap(microSecondsSinceEpoch_, that.microSecondsSinceEpoch_);
+  }
+
+  // default copy/assignment/dtor are Okay
+
+  string toString() const;
+  string toFormattedString(bool showMicroseconds = true) const;
+
+  bool valid() const { return microSecondsSinceEpoch_ > 0; }
+
+  // for internal usage.
+  int64_t microSecondsSinceEpoch() const { return microSecondsSinceEpoch_; }
+  time_t secondsSinceEpoch() const
+  { return static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond); }
+
+  ///
+  /// Get time of now.
+  ///
+  static Timestamp now();
+  static Timestamp invalid()
+  {
+    return Timestamp();
+  }
+
+  static Timestamp fromUnixTime(time_t t)
+  {
+    return fromUnixTime(t, 0);
+  }
+
+  static Timestamp fromUnixTime(time_t t, int microseconds)
+  {
+    return Timestamp(static_cast<int64_t>(t) * kMicroSecondsPerSecond + microseconds);
+  }
+
+  static const int kMicroSecondsPerSecond = 1000 * 1000;
+
+ private:
+  int64_t microSecondsSinceEpoch_;
+};
+
+inline bool operator<(Timestamp lhs, Timestamp rhs)
+{
+  return lhs.microSecondsSinceEpoch() < rhs.microSecondsSinceEpoch();
+}
+
+inline bool operator==(Timestamp lhs, Timestamp rhs)
+{
+  return lhs.microSecondsSinceEpoch() == rhs.microSecondsSinceEpoch();
+}
+
+///
+/// Gets time difference of two timestamps, result in seconds.
+///
+/// @param high, low
+/// @return (high-low) in seconds
+/// @c double has 52-bit precision, enough for one-microsecond
+/// resolution for next 100 years.
+inline double timeDifference(Timestamp high, Timestamp low)
+{
+  int64_t diff = high.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
+  return static_cast<double>(diff) / Timestamp::kMicroSecondsPerSecond;
+}
+
+///
+/// Add @c seconds to given timestamp.
+///
+/// @return timestamp+seconds as Timestamp
+///
+inline Timestamp addTime(Timestamp timestamp, double seconds)
+{
+  int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+  return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
+}
+
+}
+#endif  // MUDUO_BASE_TIMESTAMP_H
